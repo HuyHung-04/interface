42ccea18f860359ea06a3509f7e4876c357ece84
Hoàng Huy Hùng <hoanghuyhung080704@gmail.com>
2024-12-24 23:53:28 +0700

    add interface fe

--- dream-fronend/dream-angular/src/app/app.config.ts
+++ dream-fronend/dream-angular/src/app/app.config.ts
@@ -1,17 +1,27 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
-
 import { importProvidersFrom } from '@angular/core';
-import { CommonModule } from '@angular/common'; // Thêm CommonModule để sử dụng các tính năng chung của Angular.
-
+import { CommonModule } from '@angular/common';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { RouterModule } from '@angular/router';
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideClientHydration(withEventReplay()),
-    importProvidersFrom(CommonModule) // Không cần import ChartsModule, chỉ cần CommonModule
+    importProvidersFrom(
+      CommonModule,
+      BrowserAnimationsModule,
+      RouterModule,
+      MatSidenavModule,
+      MatToolbarModule,
+      MatButtonModule,
+      MatIconModule
+    )
   ]
 };
